a233dfca23577e213c6e4611e5dfbcdd3f3c053a
Angela Restrepo <Angelita_r7@hotmail.com>
2017-10-16 23:32:24 -0500

    Procesador_3

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/GEORGE/Desktop/Procesador_3/Procesador_3/PSR_TestBench_isim_beh.exe" -prj "C:/Users/GEORGE/Desktop/Procesador_3/Procesador_3/PSR_TestBench_beh.prj" "work.PSR_TestBench" 
+-intstyle "ise" -incremental -o "C:/Users/GEORGE/Desktop/Procesador_3/Procesador_3/PSR_Modifier_TestBench_isim_beh.exe" -prj "C:/Users/GEORGE/Desktop/Procesador_3/Procesador_3/PSR_Modifier_TestBench_beh.prj" "work.PSR_Modifier_TestBench" 
